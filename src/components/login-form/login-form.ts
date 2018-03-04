16b2c4a082358163aa835952722fb7c748c4d669
Osama Aboukoura <osama.aboukoura@kcl.ac.uk>
2018-03-04 16:19:23 +0000

    Add Firebase Authentication Login functionality

--- src/components/login-form/login-form.ts
+++ src/components/login-form/login-form.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from "ionic-angular";
+import { AngularFireAuth } from 'angularfire2/auth'
+import { Account } from '../../models/account/account.interface'
 
 @Component({
   selector: 'app-login-form',
@@ -7,12 +9,25 @@ import { NavController } from "ionic-angular";
 })
 export class LoginFormComponent {
 
-  constructor(private navCtrl:NavController) {
+  account = {} as Account;
+
+  constructor(private navCtrl:NavController,
+              private afAuth: AngularFireAuth) {
     console.log('Hello LoginFormComponent Component');
   }
 
   navigateToPage(pageName: string){
     pageName === 'TabsPage' ? this.navCtrl.setRoot(pageName): this.navCtrl.push(pageName);
+  }
+
+  async login(){
+    try {
+      const result = await this.afAuth.auth.signInWithEmailAndPassword(this.account.email, this.account.password);
+      console.log(result);
+
+    } catch (e) {
+      console.error(e);
+    }
   }
 
 }
--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { AngularFireAuthModule } from 'angularfire2/auth'
 import { LoginPage } from './login';
 import { ComponentsModule } from '../../components/components.module'
 
@@ -9,7 +10,8 @@ import { ComponentsModule } from '../../components/components.module'
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
-    ComponentsModule
+    ComponentsModule,
+    AngularFireAuthModule
   ],
 })
 export class LoginPageModule {}
